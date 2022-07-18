94a17ed671b358ebc2e4485cff71591092a8e573
Sam Martin <samjackmartin@gmail.com>
2022-07-18 19:34:11 +0100

    Add goodbye frontend

---
+++ client/src/pages/Goodbye.js
@@ -0,0 +1,14 @@
+const Goodbye = () => (
+	<main role="main">
+		<div>
+			<h1>Say Goodbye</h1>
+			<p>
+				Starter kit for full-stack JavaScript projects. For more information,
+				see the wiki:
+			</p>
+			<a href="/api/goodbye/Sam">Say goodbye to Sam</a>
+		</div>
+	</main>
+);
+
+export default Goodbye;
