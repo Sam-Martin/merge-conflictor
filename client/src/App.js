eb718a97f07e8d4779936247342b6b3df1f32377
Sam Martin <samjackmartin@gmail.com>
2022-07-18 19:27:15 +0100

    Added Goodbye API Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
+import Goodbye from "./pages/Goodbye";
 import Home from "./pages/Home";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
+		<Route path="/goodbye" element={<Goodbye />} />
 	</Routes>
 );
 
